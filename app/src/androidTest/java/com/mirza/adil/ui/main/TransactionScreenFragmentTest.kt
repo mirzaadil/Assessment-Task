0d4a64584f2b5c1d0b2ceb0d3baf3dad9e297e76
Mirza Adil <madil_mirza@hotmail.com>
2021-07-18 01:11:15 +0500

    UnitTest completed

--- app/src/androidTest/java/com/mirza/adil/ui/main/TransactionScreenFragmentTest.kt
+++ app/src/androidTest/java/com/mirza/adil/ui/main/TransactionScreenFragmentTest.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import androidx.test.platform.app.InstrumentationRegistry
@@ -34,20 +35,142 @@ class TransactionScreenFragmentTest {
     @Before
     fun setUp() {
         hiltRule.inject()
+        navController = mock(NavController::class.java)
     }
     @ExperimentalCoroutinesApi
     @Test
     fun checkDefaultValueForMaskNumberEditText_returnAED0_point_00(){
-        navController = mock(NavController::class.java)
-
         launchFragmentInHiltContainer<TransactionScreenFragment>(){
             Navigation.setViewNavController(requireView(),navController)
         }
         val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
-        val name = maskNumberEditText.getText()
-        assertEquals("AED0.00",name)
+        val value = maskNumberEditText.getText()
+        assertEquals("AED0.00",value)
     }
-    
+    /**
+     * The purpose of this test case is to check when we enter number in thousands, it should returns comma separated
+     * formatted string
+    * */
+
+    @Test
+    fun checkClickOnButton4561_returnAED4_comma_561_point_00(){
+        launchFragmentInHiltContainer<TransactionScreenFragment>(){
+            Navigation.setViewNavController(requireView(),navController)
+        }
+        val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        val value = maskNumberEditText.getText()
+        assertEquals("AED4,561.00",value)
+    }
+
+    /**
+     * The purpose of this test case is to check when we enter number in tens of thousands, it should returns comma separated
+     * formatted string
+     * */
+    @Test
+    fun checkClickOnButton94561_returnAED94_comma_561_point_00(){
+        launchFragmentInHiltContainer<TransactionScreenFragment>(){
+            Navigation.setViewNavController(requireView(),navController)
+        }
+        val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
+        onView(withId(R.id.button9)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        val value = maskNumberEditText.getText()
+        assertEquals("AED94,561.00",value)
+    }
+    /**
+     * The purpose of this test case is to check when we enter the number in lacs, it should
+     * return comma separated formatted string
+     * */
+    @Test
+    fun checkClickOnButton945611_returnAED945_comma_611_point_00(){
+        launchFragmentInHiltContainer<TransactionScreenFragment>(){
+            Navigation.setViewNavController(requireView(),navController)
+        }
+        val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
+        onView(withId(R.id.button9)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        val value = maskNumberEditText.getText()
+        assertEquals("AED945,611.00",value)
+    }
+    /**
+     * The purpose of this test case is to check when we enter the number in tens of lacs(millions), it should
+     * return comma separated formatted string
+     * */
+    @Test
+    fun checkClickOnButton9456123_returnAED9_comma_456_comma_123_point_00(){
+        launchFragmentInHiltContainer<TransactionScreenFragment>(){
+            Navigation.setViewNavController(requireView(),navController)
+        }
+        val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
+        onView(withId(R.id.button9)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.button3)).perform(click())
+        val value = maskNumberEditText.getText()
+        assertEquals("AED9,456,123.00",value)
+    }
+    /**
+     * The purpose of this test case is to check when we enter the number in cror, it should
+     * return comma separated formatted string
+     * */
+    @Test
+    fun checkClickOnButton94561234_returnAED94_comma_561_comma_234_point_00(){
+        launchFragmentInHiltContainer<TransactionScreenFragment>(){
+            Navigation.setViewNavController(requireView(),navController)
+        }
+        val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
+        onView(withId(R.id.button9)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.button3)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        val value = maskNumberEditText.getText()
+        assertEquals("AED94,561,234.00",value)
+    }
+    /**
+     * The purpose of this test case is to check when we enter the number in cror with decimal point values, it should
+     * return comma separated formatted string
+     * */
+    @Test
+    fun checkClickOnButton94561234dot78_returnAED94_comma_561_comma_234_point_78(){
+        launchFragmentInHiltContainer<TransactionScreenFragment>(){
+            Navigation.setViewNavController(requireView(),navController)
+        }
+        val maskNumberEditText: ViewInteraction = onView(withId(R.id.etPassCodeText))
+        onView(withId(R.id.button9)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.button3)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.buttonDot)).perform(click())
+        onView(withId(R.id.button7)).perform(click())
+        onView(withId(R.id.button8)).perform(click())
+
+        val value = maskNumberEditText.getText()
+        assertEquals("AED94,561,234.78",value)
+    }
+
+
 
     @After
     fun tearDown() {
